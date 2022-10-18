f3ba06ca6debdaf003d58d6cc0658a75cb6583b3
Brian Washington <bw49078@gmail.com>
2022-10-18 17:54:00 -0500

    finished

--- all-dragons.js
+++ all-dragons.js
@@ -1,5 +1,14 @@
 // Your code here
+const EvilDragon = require("./classes/evil-dragon")
+const { getDragons } = require("./classes/friendly-dragon")
+const FriendlyDragon = require("./classes/friendly-dragon")
 
+const falkor = new FriendlyDragon("Falkor", "white", ["save Atreyu from the swamp", "save Atreyu from the Nothing", "scare the local bullies into a dumpster"], "Bastian")
+
+const smaug = new EvilDragon("Smaug", "black", ["take over your mountain kingdom", "steal all your dwarven gold", "burn down your floating village"], "Dwarf King")
+
+let allDragons = getDragons(falkor, smaug);
+module.exports.allDragons = allDragons;
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,25 @@
 // Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...dragons) {
+    let dragonArray = [];
+
+    dragons.forEach(dragon => {
+      dragonArray.push(dragon.name);
+    })
+
+    return dragonArray;
+  }
+}
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
